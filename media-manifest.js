55593b3d336148b24ac972727bc8c3457d90280e
my-govuz <statnikovsamir@gmail.com>
2026-05-30 00:00:57 +0500

    Create romantic anniversary site

---
+++ media-manifest.js
@@ -0,0 +1,17 @@
+window.LOVE_MEDIA = [
+  {
+    type: 'placeholder',
+    title: 'Добавь первое фото Парижон',
+    caption: 'Положи файл в images/ и замени этот элемент на { type: "image", src: "images/photo-1.jpg" }.'
+  },
+  {
+    type: 'placeholder',
+    title: 'Добавь совместное фото',
+    caption: 'Лучше всего смотрятся вертикальные кадры 9:16 или нежные портреты.'
+  },
+  {
+    type: 'placeholder',
+    title: 'Добавь видео-воспоминание',
+    caption: 'Положи mp4/webm в videos/ и укажи { type: "video", src: "videos/moment.mp4" }.'
+  }
+];
---
+++ videos/README.md
@@ -0,0 +1,7 @@
+# videos
+
+Добавьте сюда mp4/webm-видео или кружочки, затем укажите их в `media-manifest.js`:
+
+```js
+{ type: 'video', src: 'videos/moment.mp4', title: 'Авиация', caption: 'Любимая сцена' }
+```
